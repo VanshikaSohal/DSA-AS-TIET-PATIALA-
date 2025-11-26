2edd3abef7bb72431843ad476c0b228b1ae89525
Vanshika Sohal <vanshika07sohal@gmail.com>
2025-11-26 22:26:02 +0530

    COMMIT

---
+++ ASSIGNMENT9/Q1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+void bfs(int start, int adj[][10], int adjSize[], bool visited[], int n) {
+    int queue[100]; 
+    int front = 0, rear = 0;
+    queue[rear++] = start;
+    visited[start] = true;
+    while (front != rear) {
+        int node = queue[front++];
+        cout << node << " ";
+        for (int i = 0; i < adjSize[node]; i++) {
+            int neighbor = adj[node][i];
+            if (!visited[neighbor]) {
+                queue[rear++] = neighbor;
+                visited[neighbor] = true;
+            }
+        }
+    }
+}
+int main() {
+    int n = 6;
+    int adj[10][10];   
+    int adjsize[10];   
+    for (int i = 0; i < n; i++) 
+	adjsize[i] = 0;
+    adj[0][adjsize[0]++] = 1;
+    adj[1][adjsize[1]++] = 0;
+    adj[0][adjsize[0]++] = 2;
+    adj[2][adjsize[2]++] = 0;
+    adj[1][adjsize[1]++] = 3;
+    adj[3][adjsize[3]++] = 1;
+    adj[2][adjsize[2]++] = 4;
+    adj[4][adjsize[4]++] = 2;
+    adj[3][adjsize[3]++] = 5;
+    adj[5][adjsize[5]++] = 3;
+    bool visited[10];
+    for (int i = 0; i < n; i++)
+	visited[i] = false;
+    cout << "BFS traversal starting from node:";
+    cout<<endl;
+    bfs(0, adj, adjsize, visited, n);
+    return 0;
+}
+
---
+++ ASSIGNMENT9/Q4.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+const int MAX = 10;
+int n;
+int graph[MAX][MAX];
+bool visited[MAX];
+int dist[MAX];
+int distmin() {
+    int minval = 9999, minindex = -1;
+    for (int i = 0; i < n; i++) {
+        if (!visited[i] && dist[i] < minval) {
+            minval = dist[i];
+            minindex = i;
+        }
+    }
+    return minindex;
+}
+void dijkstra(int start) {
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
+        dist[i] = 9999;
+    }
+    dist[start] = 0;
+    for (int count = 0; count < n - 1; count++) {
+        int u = distmin();
+        if (u == -1) break;
+        visited[u] = true;
+        for (int v = 0; v < n; v++) {
+            if (!visited[v] && graph[u][v] && dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+    cout << "Shortest distances from node " << start << endl;
+    for (int i = 0; i < n; i++) {
+        cout << i << " : " << dist[i] << endl;
+    }
+}
+int main() {
+    cout << "Enter number of nodes: ";
+    cin >> n;
+    cout << "Enter adjacency matrix:";
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> graph[i][j];
+    int start;
+    cout << "Enter starting node: ";
+    cin >> start;
+    dijkstra(start);
+    return 0;
+}
