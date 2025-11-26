d726c1d5c12237b9a1443ed7ffffddcd9c808e2d
Vanshika Sohal <vanshika07sohal@gmail.com>
2025-11-26 21:51:37 +0530

    Q8.cpp

--- ASSIGNMENT2/Q8.cpp
+++ ASSIGNMENT2/Q8.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-#include <algorithm> 
 using namespace std;
 int distinctcount(int arr[], int n) {
-    sort(arr, arr + n); 
-    int count = 1; 
-    for (int i = 1; i < n; i++) {
-        if (arr[i] != arr[i - 1]) {
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {     
+                int temp = arr[j];        
+                arr[j] = arr[j + 1];      
+                arr[j + 1] = temp;        
+            }
+        }
+    }
+    int count = 1;  
+    for(int i = 1; i < n; i++) {
+        if(arr[i] != arr[i - 1]) {
             count++;
         }
     }
@@ -24,4 +31,3 @@ int main() {
     distinctcount(arr, n);
     return 0;
 }
-
