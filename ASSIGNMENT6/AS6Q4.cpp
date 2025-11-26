94000e05e01ed062a5e23d8de8ac263a6b419032
Vanshika Sohal <vanshika07sohal@gmail.com>
2025-11-26 22:07:03 +0530

    AS6Q4.cpp

--- ASSIGNMENT6/AS6Q4.cpp
+++ ASSIGNMENT6/AS6Q4.cpp
@@ -1,46 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-struct DNode{
+struct DNode {
     char data;
     DNode* next;
     DNode* prev;
 };
 DNode* head = NULL;
-void insertLast(char val){
+void insertLast(char val) {
     DNode* n = new DNode;
     n->data = val;
     n->next = NULL;
     n->prev = NULL;
-    if(!head){
+    if (!head) {
         head = n;
         return;
     }
     DNode* temp = head;
-    while(temp->next) temp = temp->next;
+    while (temp->next) temp = temp->next;
     temp->next = n;
     n->prev = temp;
 }
-bool isPalindrome(){
-    if(!head || !head->next) return true;
+bool isPalindrome() {
+    if (!head || !head->next) 
+		return true;
     DNode* left = head;
     DNode* right = head;
-    while(right->next) right = right->next;
-    while(left != right && right->next != left){
-        if(left->data != right->data) return false;
+    while (right->next) 
+		right = right->next;
+    while (left != right && left->prev != right) {
+        if (left->data != right->data) return false;
         left = left->next;
         right = right->prev;
     }
     return true;
 }
-void display(){
+void display() {
     DNode* temp = head;
-    while(temp){
-        cout << temp->data << " ";
+    while (temp) {
+        cout << temp->data;
         temp = temp->next;
     }
     cout << endl;
 }
-int main(){
+int main() {
     insertLast('m');
     insertLast('a');
     insertLast('d');
@@ -48,10 +50,9 @@ int main(){
     insertLast('m');
     cout << "Doubly Linked List: ";
     display();
-    if(isPalindrome()) 
-	cout << "Palindrome: True" << endl;
-    else 
-	cout << "Palindrome: False" << endl;
+    if (isPalindrome())
+        cout << "Palindrome" << endl;
+    else
+        cout << " Not a Palindrome" << endl;
     return 0;
 }
-
